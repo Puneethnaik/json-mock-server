0116de3f05d78d4dc93a8a5de14e993e09b77157
puneeth <puneethn250@gmail.com>
2020-10-04 12:05:23 +0530

    datastore API: rollBack, update, remove, partialUpdate, find

--- datastore_access.js
+++ datastore_access.js
@@ -1,49 +1,338 @@
-const fs = require("fs");
-const errors = require("./datastore_errors");
+/* eslint-disable eqeqeq */
+const fs = require('fs')
+const path = require('path')
 
-let find = function(entity, conditions) {
-    let isEntityPresent = entityPresent(entity);
-    if(!isEntityPresent) {
-        throw new errors.EntityNotPresentError;
-    }
-    let datastore = fs.readFile("datastore/store.json", (err, data) => {
-        if(err) {
-            throw err;
-        }
-        //get the JSON of the data from the file
-        data = JSON.parse(data);
-        let entities = data[entity];
-        if(conditions) {
-            entities.filter((entity) => {
-                let shouldInclude = true;
-                for(let condition in conditions) {
-                    shouldInclude &= (entity[condition] === conditions[condition]);
+const errors = require('./datastore_errors')
+
+const find = (entity, conditions) => {
+    return new Promise((resolve, reject) => {
+        entityPresent(entity).then((isEntityPresent) => {
+            if (!isEntityPresent) {
+                reject(new errors.EntityNotPresentError())
+                return
+            }
+            fs.readFile('datastore/store.json', (err, data) => {
+                if (err) {
+                    reject(new errors.TransactionFailedError())
+                    return
                 }
-                return shouldInclude;
+                // get the JSON of the data from the file
+                data = JSON.parse(data)
+                let entities = data[entity]
+                if (conditions) {
+                    for (const condition in conditions) {
+                        try {
+                            conditions[condition] = JSON.parse(conditions[condition])
+                        } catch (e) {
+                            if (e instanceof SyntaxError) {
+                                // condition is not an object
+                            } else {
+                                reject(new errors.TransactionFailedError())
+                                return
+                            }
+                        }
+                    }
+                    entities = entities.filter((entity) => {
+                        let shouldInclude = true
+                        for (const condition in conditions) {
+                            if (condition.indexOf('_') == -1) {
+                                // we check if the condition given is an object or not
+                                shouldInclude &= (isEqual(entity, condition, conditions[condition]))
+                            }
+                        }
+                        return shouldInclude
+                    })
+                    // sort the dataset if _sort is defined
+                    if (conditions._sort) {
+                        if (conditions._order) {
+                            if (conditions._order === 'asc') {
+                                entities.sort((A, B) => {
+                                    if (A[conditions._sort] < B[conditions._sort]) {
+                                        return -1
+                                    } else {
+                                        return 1
+                                    }
+                                })
+                            } else if (conditions._order === 'desc') {
+                                entities.sort((A, B) => {
+                                    if (A[conditions._sort] > B[conditions._sort]) {
+                                        return -1
+                                    } else {
+                                        return 1
+                                    }
+                                })
+                            }
+                        } else {
+                            // default order is asc if _sort is defined.
+                            entities.sort((A, B) => {
+                                if (A[conditions._sort] < B[conditions._sort]) {
+                                    return -1
+                                } else {
+                                    return 1
+                                }
+                            })
+                        }
+                    }
+                }
+                resolve(entities)
             })
-            
-        } else {
-        }
-        return new Promise(resolve => {
-            resolve(entities);
+        }, (err) => {
+            console.log(err)
+            reject(new errors.TransactionFailedError())
         })
     })
 }
-let entityPresent = (entity) => {
-  let metadata = fs.readFile("datastore/metadata.json", (err, metaData) => {
-    if(err) {
-        throw err;
-    }
-    metaData = JSON.parse(metaData);
-    if(metaData[entity] !== undefined) {
-        return false;
+const isEqual = (entity, conditionName, conditionValue) => {
+    const positionOfFirstDotOperator = conditionName.indexOf('.')
+    if (positionOfFirstDotOperator !== -1) {
+        const keyToFilterOn = conditionName.slice(0, positionOfFirstDotOperator)
+        return isEqual(entity[keyToFilterOn], conditionName.slice(positionOfFirstDotOperator + 1), conditionValue)
     } else {
-        return true;
+        if (!entity || !entity[conditionName]) {
+            return false
+        }
+        if (conditionValue instanceof Object) {
+            return JSON.stringify(entity[conditionName]) == JSON.stringify(conditionValue)
+        } else {
+            return entity[conditionName] == conditionValue
+        }
     }
-  })  
+}
+
+const entityPresent = (entity) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path.join(__dirname, 'datastore/metadata.json'), (err, metaData) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            metaData = JSON.parse(metaData)
+            resolve(metaData[entity] !== undefined)
+        })
+    })
+}
+
+const insert = (entity, item) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path.join(__dirname, 'datastore/metadata.json'), (err, metaData) => {
+            if (err) {
+                reject(new errors.TransactionFailedError())
+                return
+            }
+            metaData = JSON.parse(metaData)
+            const newMetaData = JSON.parse(JSON.stringify(metaData))
+            if (!newMetaData[entity]) {
+                newMetaData[entity] = true
+            }
+            fs.readFile(path.join(__dirname, 'datastore/store.json'), (err, data) => {
+                if (err) {
+                    reject(new errors.TransactionFailedError())
+                    return
+                }
+                data = JSON.parse(data)
+                if (!data[entity]) {
+                    data[entity] = []
+                }
+                for (const subEntity of data[entity]) {
+                    if (subEntity.id == item.id) {
+                        reject(new errors.IntegrityConstraintViolationError())
+                        return
+                    }
+                }
+                data[entity].push(item)
+                fs.writeFile(path.join(__dirname, 'datastore/metadata.json'), JSON.stringify(newMetaData), (err) => {
+                    if (err) {
+                        reject(new errors.TransactionFailedError())
+                        return
+                    }
+                    fs.writeFile(path.join(__dirname, 'datastore/store.json'), JSON.stringify(data), (err) => {
+                        if (err) {
+                            rollBack(path.join(__dirname, 'datastore/metadata.json'), metaData)
+                            reject(new errors.TransactionFailedError())
+                            return
+                        }
+                        resolve('OK')
+                    })
+                })
+            })
+        })
+    })
+}
+
+const rollBack = (fileName, oldData) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, oldData, (err) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve('Rollback successful')
+        })
+    })
+}
+
+const update = (entity, item) => {
+    return new Promise((resolve, reject) => {
+        entityPresent(entity).then(isEntityPresent => {
+            if (!isEntityPresent) {
+                reject(new errors.EntityNotPresentError())
+                return
+            }
+            fs.readFile('datastore/store.json', (err, data) => {
+                if (err) {
+                    reject(err)
+                    return
+                }
+                data = JSON.parse(data)
+                const entities = data[entity]
+                let entityFound = false
+                for (const key in entities) {
+                    if (entities[key].id == item.id) {
+                        entityFound = true
+                        entities[key] = item
+                        break
+                    }
+                }
+                if (!entityFound) {
+                    reject(new errors.NoSuchEntityError())
+                    return
+                }
+                data[entity] = entities
+                fs.writeFile('datastore/store.json', JSON.stringify(data), (err) => {
+                    if (err) {
+                        reject(new errors.TransactionFailedError())
+                        return
+                    }
+                    resolve('OK')
+                })
+            })
+        }, _ => {
+            reject(new errors.TransactionFailedError())
+        })
+    })
+}
+
+const partialUpdate = (entity, item) => {
+    return new Promise((resolve, reject) => {
+        entityPresent(entity).then(isEntityPresent => {
+            if (!isEntityPresent) {
+                reject(new errors.EntityNotPresentError())
+                return
+            }
+            fs.readFile('datastore/store.json', (err, data) => {
+                if (err) {
+                    reject(new errors.TransactionFailedError())
+                }
+                data = JSON.parse(data)
+                const entities = data[entity]
+                let entityFound = false
+                for (const key in entities) {
+                    if (entities[key].id == item.id) {
+                        entityFound = true
+                        const entityToUpdate = entities[key]
+                        for (const key in item) {
+                            entityToUpdate[key] = item[key]
+                        }
+                        break
+                    }
+                }
+                if (!entityFound) {
+                    reject(new errors.NoSuchEntityError())
+                    return
+                }
+                data[entity] = entities
+                fs.writeFile('datastore/store.json', JSON.stringify(data), (err) => {
+                    if (err) {
+                        reject(new errors.TransactionFailedError())
+                        return
+                    }
+                    resolve('OK')
+                })
+            })
+        }, _ => {
+            reject(new errors.TransactionFailedError())
+        })
+    })
+}
+
+const remove = (entity, id) => {
+    return new Promise((resolve, reject) => {
+        entityPresent(entity).then(isEntityPresent => {
+            if (!isEntityPresent) {
+                reject(new errors.EntityNotPresentError())
+                return
+            }
+            fs.readFile('datastore/store.json', (err, data) => {
+                if (err) {
+                    reject(new errors.TransactionFailedError())
+                    return
+                }
+                data = JSON.parse(data)
+                const entities = data[entity]
+                let entityFound = false
+                let entityToDelete
+                for (let i = 0; i < entities.length; i++) {
+                    if (entities[i].id == id) {
+                        entityFound = true
+                        entityToDelete = i
+                        break
+                    }
+                }
+                if (!entityFound) {
+                    reject(new errors.NoSuchEntityError())
+                    return
+                }
+                entities.splice(entityToDelete, 1)
+                if (entities.length === 0) {
+                    fs.readFile(path.join(__dirname, 'datastore/metadata.json'), (err, metaData) => {
+                        if (err) {
+                            reject(new errors.TransactionFailedError())
+                            return
+                        }
+                        metaData = JSON.parse(metaData)
+                        const newMetaData = JSON.parse(JSON.stringify(metaData))
+                        delete newMetaData[entity]
+                        delete data[entity]
+                        fs.writeFile('datastore/metadata.json', JSON.stringify(newMetaData), (err) => {
+                            if (err) {
+                                reject(new errors.TransactionFailedError())
+                                return
+                            }
+                            fs.writeFile('datastore/store.json', JSON.stringify(data), (err) => {
+                                if (err) {
+                                    rollBack('datastore/metadata.json', metaData).then(result => {
+                                        reject(new errors.TransactionFailedError())
+                                    }, _ => {
+                                        reject(new errors.TransactionFailedError())
+                                    })
+                                } else {
+                                    resolve('OK')
+                                }
+                            })
+                        })
+                    })
+                } else {
+                    data[entity] = entities
+                    fs.writeFile('datastore/store.json', JSON.stringify(data), (err) => {
+                        if (err) {
+                            reject(new errors.TransactionFailedError())
+                            return
+                        }
+                        resolve('OK')
+                    })
+                }
+            })
+        }, _ => {
+            reject(new errors.TransactionFailedError())
+        })
+    })
 }
 
 module.exports = {
     find,
+    insert,
+    update,
+    remove,
+    partialUpdate,
     entityPresent
 }
