dac8e614dd9571bea757f9059deddaf8a61a1705
puneeth <puneethn250@gmail.com>
2020-10-04 12:06:18 +0530

    EntityNotPresent, NoSuchEntity, TransactionFailed, IntegrityConstraintViolation errors

--- datastore_errors.js
+++ datastore_errors.js
@@ -1,10 +1,34 @@
 class EntityNotPresentError extends Error {
-    constructor() {
-        super();
-        this.message = "The requested entity is not found";
+    constructor () {
+        super()
+        this.message = 'The requested entity is not found in metadata.'
+    }
+}
+
+class NoSuchEntityError extends Error {
+    constructor () {
+        super()
+        this.message = 'The requested entity is not found in the store.'
+    }
+}
+
+class TransactionFailedError extends Error {
+    constructor () {
+        super()
+        this.message = 'The transaction has failed.'
+    }
+}
+
+class IntegrityConstraintViolationError extends Error {
+    constructor () {
+        super()
+        this.message = 'Performing an operation caused an integrity constraint violation.'
     }
 }
 
 module.exports = {
-    EntityNotPresentError
+    EntityNotPresentError,
+    NoSuchEntityError,
+    TransactionFailedError,
+    IntegrityConstraintViolationError
 }
